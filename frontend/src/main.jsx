53b8b7bd9d605bf18864fe27f4dedb3496a4feb0
shania-codes <sh@ni.a>
2025-11-24 07:57:46 +0000

    delete merged react fails

--- frontend/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RecipeApp from "./RecipeApp.jsx";
-
-const rootDiv = document.getElementById("recipe-root");
-const recipes = JSON.parse(rootDiv.dataset.recipes);
-ReactDOM.createRoot(rootDiv).render(<RecipeApp recipes={recipes} />);
